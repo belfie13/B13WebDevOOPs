2857e9728f98a283e83a1ee301b9732c4d079399
belfie13 <39270867+belfie13@users.noreply.github.com>
2018-05-26 23:01:58 +1000

    Create phptest.php

---
+++ phptest.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8" />
+  <title>___TITLE___</title>
+
+</head>
+
+<body>
+
+  <?php include 'header.php';?>
+  
+  <main></main>
+
+</body>
+
+</html>
